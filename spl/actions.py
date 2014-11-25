522badbee23efa34e09b48f3d2fe9714544a64b0
ilker <ilker@pisi_linux1.0>
2014-11-25 21:04:26 +0100

    actions.py changed

--- spl/actions.py
+++ spl/actions.py
@@ -7,14 +7,12 @@
 from pisi.actionsapi import autotools
 from pisi.actionsapi import pisitools
 from pisi.actionsapi import shelltools
+from pisi.actionsapi import kerneltools
 
 def setup():
-
     shelltools.system("./autogen.sh")
-    autotools.configure("--with-linux=/usr/src/linux-headers-%s \
-            --with-gcc=%s \
-            --with-g++=%s \
-            " % (KDIR, get.CC(), get.CXX())))
+    autotools.configure()
+    #autotools.configure("--with-linux=/usr/src/linux-headers-%s --with-gcc=%s --with-g++=%s"  % (kerneltools.getKernelVersion(), get.CC(), get.CXX())))
 
 def build():
     autotools.make()
